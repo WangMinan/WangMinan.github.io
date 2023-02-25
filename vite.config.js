f1eab20d93f2bead14202dfe49b3d8fb16661b74
WangMinan <wangminan0811@hotmail.com>
2023-02-25 10:50:42 +0800

    测试更新了相对路径问题

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,12 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: './'
+  server: {
+    port: 7070,
+    // host: '0.0.0.0'
+  },
+  preview: {
+    port: 7070
+  },
+  base: './',
 })
